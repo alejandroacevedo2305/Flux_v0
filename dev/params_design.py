aee2be2350753d2981125c7e7bd78db5338c29d9
Alejandro Acevedo <a.acevedo.aracena@gmail.com>
2023-10-13 17:57:45 -0300

    optuna okey

--- dev/params_design.py
+++ dev/params_design.py
@@ -4,6 +4,12 @@ from itertools import chain, combinations
 import math
 from src.simulador_v02 import *  
 from src.gymnasium_utils import *  
+from scipy.stats import gmean
+def non_empty_subsets(lst):
+    return list(chain.from_iterable(combinations(lst, r) for r in range(1, len(lst) + 1)))
+
+
+
 
 un_dia   = forecast().un_mes().un_dia().df.sort_values(by='FH_Emi', inplace=False)
 sucursal = 2
@@ -12,55 +18,62 @@ skills   = obtener_skills(un_dia)
 series   = sorted(list({val for sublist in skills.values() for val in sublist}))
 SLAs     = [(0.84, 10), (0.34, 25), (0.8, 33)]
 niveles_servicio_x_serie = {s:random.choice(SLAs) for s in series}
-modos_atenciones=["Alternancia", "FIFO", "Rebalse"]
-max_escritorios = len(skills.keys())
+# modos_atenciones=["Alternancia", "FIFO", "Rebalse"]
+# max_escritorios = len(skills.keys())
 
-prioridades = prioridad_x_serie(niveles_servicio_x_serie,2,1) 
+# prioridades = prioridad_x_serie(niveles_servicio_x_serie,2,1) 
 
-action_space = get_action_space(
-    skills     = skills, 
-    config_set = modos_atenciones, 
-    time_start = str(un_dia.FH_Emi.min().time()), 
-    time_end   = str(un_dia.FH_Emi.max().time())
-)
+# action_space = get_action_space(
+#     skills     = skills, 
+#     config_set = modos_atenciones, 
+#     time_start = str(un_dia.FH_Emi.min().time()), 
+#     time_end   = str(un_dia.FH_Emi.max().time())
+# )
 
-plan = convert_output(action_space.sample(), skills_set = series, config_set = modos_atenciones)
-# registros_SLA =   simular(plan, niveles_servicio_x_serie, un_dia, prioridades)   
-from scipy.stats import gmean
+# plan = convert_output(action_space.sample(), skills_set = series, config_set = modos_atenciones)
 
-#gmean(registros_SLA .drop("hora", axis=1).iloc[-1].dropna())
 
-# Function to generate all non-empty subsets of a given list
-def non_empty_subsets(lst):
-    return list(chain.from_iterable(combinations(lst, r) for r in range(1, len(lst) + 1)))
 
 # Optuna multi-objective function
-def objective(trial, skills):
+def objective(trial, un_dia,skills, niveles_servicio_x_serie,  modos_atenciones:list = ["Alternancia", "FIFO", "Rebalse"]):
     try:
         bool_vector = [trial.suggest_categorical(f'escritorio_{i}', [True, False]) for i in range(len(skills.keys()))]  
-        modos_atenciones = ["Alternancia", "FIFO", "Rebalse"]
+        #modos_atenciones = ["Alternancia", "FIFO", "Rebalse"]
         series = sorted(list({val for sublist in skills.values() for val in sublist}))
         str_dict = {i: trial.suggest_categorical('modo atención', modos_atenciones) for i in range(len(skills.keys()))}
-        subset_dict = {'serie': trial.suggest_categorical('series', non_empty_subsets(series)) for _ in range(len(skills.keys()))}
-
+        subset_dict = {i : trial.suggest_categorical('series', non_empty_subsets(series)) for i in range(len(skills.keys()))}
         # Storing user attributes
         trial.set_user_attr('bool_vector', bool_vector)
         trial.set_user_attr('str_dict', str_dict)
         trial.set_user_attr('subset_dict', subset_dict)
 
-        SLAs = [(0.84, 10), (0.34, 25), (0.8, 33)]
-        niveles_servicio_x_serie = {s: random.choice(SLAs) for s in series}
-        prioridades = prioridad_x_serie(niveles_servicio_x_serie, 2, 1) 
+        #SLAs                     = [(0.84, 10), (0.34, 25), (0.8, 33)]
+        #niveles_servicio_x_serie = {s: random.choice(SLAs) for s in series}
+        prioridades              = prioridad_x_serie(niveles_servicio_x_serie, 2, 1) 
+        p                        = {}
+        inicio                   = str(un_dia.FH_Emi.min().time())#'08:33:00'
+        termino                  =  str(un_dia.FH_Emi.max().time())#'14:33:00'
+        # Loop through the keys
+        for key in str_dict.keys():
+            # Apply boolean mask
+            if bool_vector[key]:
+                # Create the inner dictionary
+                inner_dict = {
+                    'inicio': inicio,
+                    'termino': termino,
+                    'propiedades': {
+                        'skills': list(subset_dict[key]),
+                        'configuracion_atencion': str_dict[key]
+                    }
+                }
+                # Convert integer key to string and add the inner dictionary to a list, then add it to the output dictionary
+                p[str(key)] = [inner_dict]
         
-        action_space = get_action_space(
-            skills={k: v for k, v, mask in zip(skills.keys(), skills.values(), bool_vector) if mask},
-            config_set=modos_atenciones, 
-            time_start=str(un_dia.FH_Emi.min().time()), 
-            time_end=str(un_dia.FH_Emi.max().time())
-        )
+        registros_SLA = simular(p, niveles_servicio_x_serie, un_dia, prioridades)
         
-        plan = convert_output(action_space.sample(), skills_set=series, config_set=modos_atenciones)
-        registros_SLA = simular(plan, niveles_servicio_x_serie, un_dia, prioridades)
+        print(f"{str_dict}")
+        print(f"{subset_dict}")
+
         
         return gmean(registros_SLA.drop("hora", axis=1).iloc[-1].dropna()), sum(bool_vector)
 
@@ -71,11 +84,9 @@ def objective(trial, skills):
 
 study = optuna.multi_objective.create_study(directions=['maximize', 'minimize'])
 study.optimize(lambda trial: objective(trial, 
-                                       skills = skills,
-                                       #max_escritorios          = max_escritorios, 
-                                       #series                   = series, 
-                                       #modos_atenciones         = modos_atenciones,
-                                       #niveles_servicio_x_serie = niveles_servicio_x_serie
+                                       un_dia                   = un_dia,
+                                       skills                   = skills,
+                                       niveles_servicio_x_serie = niveles_servicio_x_serie,
                                        ), 
                                        n_trials                 = 3)
 
