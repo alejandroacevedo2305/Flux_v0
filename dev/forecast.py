63d3b77b0989af97cc31cfe2bdf21d288d5f1967
Alejandro Acevedo <a.acevedo.aracena@gmail.com>
2023-10-16 20:00:43 -0300

    workforce okey

--- dev/forecast.py
+++ dev/forecast.py
@@ -1,4 +1,7 @@
 #%%
+import warnings
+warnings.filterwarnings("ignore")
+
 from datetime import date, time
 import optuna
 from itertools import chain, combinations
@@ -15,6 +18,8 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX
 from datetime import date, time
 import math
 import random 
+
+
 
 class ForecasterTTP:
 
@@ -87,6 +92,10 @@ class ForecasterTTP:
         timelimit_below = timelimit - pd.Timedelta(weeks=4)
         timelimit_upper = timelimit + pd.Timedelta(weeks=1)
 
+        self.serie_t_espera : pd.Serie = dataset.atenciones_agg[
+                (timelimit_below <= dataset.atenciones_agg["FH_Emi"]) & (dataset.atenciones_agg["FH_Emi"] <= timelimit) 
+            ].groupby(by=[ "IdSerie"]).median()["T_Ate"]
+
         demandas_proyectadas = {}
         
         for SERIE in series: 
@@ -148,6 +157,7 @@ class ForecasterTTP:
         df = df.loc[df.index.repeat(df["Demanda"])].reset_index(drop=True)
 
         df["FH_Emi"] = df["FH_Emi"].apply( lambda x : x + pd.Timedelta(seconds=random.randint(1,3599)) )
+        df["T_Ate"] = df["IdSerie"].apply( lambda x : forecaster_ttp.serie_t_espera[int(x)] ) # Cosa para incluir tiempos de atencion estimados
 
         return df.drop("Demanda", axis="columns").sort_values(by="FH_Emi").reset_index(drop=True)
 
@@ -161,28 +171,248 @@ class ForecasterTTP:
         return demandas_proyectadas_cuantizada[ (inicio_dia <= demandas_proyectadas_cuantizada["FH_Emi"]) & (demandas_proyectadas_cuantizada["FH_Emi"] <= fin_dia) ]
 
 #%%
-import warnings
-warnings.filterwarnings("ignore")
 # EJEMPLO DE USO
 dataset = DatasetTTP.desde_csv_atenciones("data/fonasa_monjitas.csv.gz")
-
-SERIES = [10, 14, 12, 17, 5, 11]
+SERIES = [5, 10, 11, 12, 14, 17]#[10, 14, 12, 17, 5, 11]
 
 forecaster_ttp = ForecasterTTP(dataset=dataset, series=SERIES, atenciones_diarias_minimas=1)
-
 # Genera una proyeccion de demanda, 
-demandas_proyectadas_q = forecaster_ttp.proyectar_demanda_SARIMAX('2023-08-15').demandas_proyectada_cuantizadas()
-demandas_proyectadas_q = ForecasterTTP.un_dia('2023-08-15', demandas_proyectadas_q) # Retorna solo un dia
-demandas_proyectadas_q
+demandas_proyectadas_q = forecaster_ttp.proyectar_demanda_SARIMAX('2023-05-15').demandas_proyectada_cuantizadas()
+demandas_proyectadas_q = ForecasterTTP.un_dia('2023-05-15', demandas_proyectadas_q) # Retorna solo un dia 2023-05-15
 
-# Instancia el generador
-#%%forecaster_ttp.generador = demandas_proyectadas_q.iterrows()
 
-# COSA PARA GENERAR GRAFICOS A TIEMPOS ARBITRARIOS
-for i, SERIE in enumerate(SERIES): 
 
-    pd.DataFrame({
-        # Empaca series para lidear con el no tener el mismo largo
-        f"{SERIE}_proyectada" : pd.Series(forecaster_ttp.demandas_proyectadas[f"{SERIE}_proyectada"]),
-        f"{SERIE}_real"       : pd.Series(forecaster_ttp.demandas_proyectadas[f"{SERIE}_real"]),
-    }).dropna().plot.line(title = f'Serie {SERIE}')
+
+#%%
+from pandas import Timestamp, Timedelta
+
+df = demandas_proyectadas_q #pd.DataFrame(data)
+
+# Task 1: Convert the dtypes
+df['IdSerie'] = df['IdSerie'].astype('Int8')  # Convert IdSerie to Int8
+df['T_Ate'] = df['T_Ate'].astype('int32')  # Convert T_Ate to int32
+df['FH_Emi'] = pd.to_datetime(df['FH_Emi'])  # Ensure FH_Emi is in datetime64[ns] format
+
+# Task 2: Create a new column FH_AteFin
+# Convert T_Ate to Timedelta in minutes, then add to FH_Emi
+df['FH_AteFin'] = df['FH_Emi'] + pd.to_timedelta(df['T_Ate'], unit='m')
+
+# Task 3: FH_AteFin is already in the same format as FH_Emi (datetime64[ns])
+
+# Task 4: Create another column FH_AteIni
+# Add 2 minutes to each entry in the FH_Emi column
+df['FH_AteIni'] = df['FH_Emi'] + Timedelta(minutes=2)
+
+# Show the DataFrame to confirm changes
+un_dia = df
+
+#%%
+
+def extract_skills_length(data):
+    result = {}
+    
+    # Initialize a variable to store the sum of all lengths.
+    total_length = 0
+    
+    # Iterate over keys and values in the input dictionary.
+    for key, entries in data.items():
+        # Initialize an empty list to store the lengths for this key.
+        lengths_for_key = []
+        
+        # Iterate over each entry which is a dictionary.
+        for entry in entries:
+            # Access the 'skills' field, and calculate its length.
+            skills_length = len(entry['propiedades']['skills'])
+            
+            # Add the current length to the total_length.
+            total_length += skills_length
+            
+            # Append this length to the list for this key.
+            lengths_for_key.append(skills_length)
+        
+        # Store the list of lengths in the result dictionary, using the same key.
+        result[key] = lengths_for_key
+    
+    # Return the result dictionary and the total sum of all lengths.
+    return total_length #, result
+def non_empty_subsets(lst):
+    return list(chain.from_iterable(combinations(lst, r) for r in range(1, len(lst) + 1)))
+skills   = {'escritorio_7': [10, 12],
+ 'escritorio_10': [17, 14],
+ 'escritorio_12': [17, 14],
+ 'escritorio_11': [17, 14],
+ 'escritorio_13': [17, 10, 14],
+ 'escritorio_2': [10, 11, 5],
+ 'escritorio_4': [10, 11, 12, 5],
+ 'escritorio_9': [10, 12, 5],
+ 'escritorio_6': [10, 5],
+ 'escritorio_5': [10, 5],
+ 'escritorio_8': [10, 12],
+ 'escritorio_1': [10, 11, 12],
+ 'escritorio_3': [10, 11]}#obtener_skills(un_dia)
+series   = sorted(list({val for sublist in skills.values() for val in sublist}))
+SLAs     = [(0.6, 30), (0.34, 35), (0.7, 45)]
+niveles_servicio_x_serie = {s:random.choice(SLAs) for s in series}
+
+
+def objective(trial, un_dia,skills, subsets, niveles_servicio_x_serie,  modos_atenciones:list = ["Alternancia", "FIFO", "Rebalse"]):
+    try:
+        bool_vector  = [trial.suggest_categorical(f'escritorio_{i}', [True, False]) for i in range(len(skills.keys()))]
+        str_dict     = {i: trial.suggest_categorical(f'{i}',         modos_atenciones) for i in range(len(skills.keys()))} 
+        subset_idx = {i: trial.suggest_int(f'ids_{i}', 0, len(subsets) - 1) for i in range(len(skills.keys()))}
+        prioridades              =  prioridad_x_serie(niveles_servicio_x_serie, 2, 1) 
+        planificacion            =  {}
+        inicio                   =  str(un_dia.FH_Emi.min().time())#'08:33:00'
+        termino                  =  str(un_dia.FH_Emi.max().time())#'14:33:00'
+        # Loop through the keys
+        for key in str_dict.keys():
+            # Apply boolean mask
+            if bool_vector[key]:
+                # Create the inner dictionary
+                inner_dict = {
+                    'inicio': inicio,
+                    'termino': termino,
+                    'propiedades': {
+                        'skills':list(subsets[subset_idx[key]]),
+                        #'skills': list(subset_dict[key]),
+                        'configuracion_atencion': str_dict[key],
+                        #'zz': 0,
+                    }
+                }
+                # Convert integer key to string and add the inner dictionary to a list, then add it to the output dictionary
+                planificacion[str(key)] = [inner_dict]
+        #print(f"---------------------------{planificacion}")
+        trial.set_user_attr('planificacion', planificacion)
+        registros_SLA = simular(planificacion, niveles_servicio_x_serie, un_dia, prioridades)       
+        
+        obj1 = gmean(registros_SLA.drop("hora", axis=1).iloc[-1].dropna())
+        obj2 = sum(bool_vector)
+        obj3 = extract_skills_length(planificacion)
+        print(f"SLA global: {obj1}, n Escritorios: {obj2}, n Series cargadas: {obj3}")
+        
+        return obj1, obj2, obj3 #gmean(registros_SLA.drop("hora", axis=1).iloc[-1].dropna()), sum(bool_vector), extract_skills_length(planificacion)
+
+    except Exception as e:
+        print(f"An exception occurred: {e}")
+        raise optuna.TrialPruned()
+
+rows = len(un_dia)
+chunk_size = rows // 4
+partitions = [un_dia.iloc[i:i + chunk_size] for i in range(0, rows, chunk_size)]
+# Create a SQLite storage to save all studies
+storage = optuna.storages.get_storage("sqlite:///multiple_studies.db")
+tramos = [f"{str(p.FH_Emi.min().time())} - {str(p.FH_Emi.max().time())}" for p in partitions]
+
+# Loop to optimize each partition
+for idx, part in enumerate(partitions):
+
+    # Create a multi-objective study object and specify the storage
+    study_name = f"workforce_{idx}"  # Unique name for each partition
+    study = optuna.multi_objective.create_study(directions=['maximize', 'minimize', 'minimize'],
+                                                study_name=study_name,
+                                                storage=storage, load_if_exists=True)
+    # Optimize the study, the objective function is passed in as the first argument
+    subsets = non_empty_subsets(sorted(list({val for sublist in skills.values() for val in sublist})))
+    study.optimize(lambda trial: objective(trial, 
+                                        un_dia                   = part,
+                                        skills                   = skills,
+                                        subsets                  = subsets,
+                                        niveles_servicio_x_serie = niveles_servicio_x_serie,
+                                        ), 
+                                        n_trials                 = 5)
+
+#%%
+#%% -------------------------------------------
+
+def regenerate_global_keys(lst_of_dicts):
+
+    new_list = []
+    
+    # Initialize a counter for the new globally unique keys
+    global_counter = 0
+    
+    # Loop through each dictionary in the original list
+    for dct in lst_of_dicts:
+        
+        # Initialize an empty dictionary to hold the key-value pairs of the original dictionary but with new keys
+        new_dct = {}
+        
+        # Loop through each key-value pair in the original dictionary
+        for key, value in dct.items():
+            
+            # Assign the value to a new key in the new dictionary
+            new_dct[global_counter] = value
+            
+            # Increment the global counter for the next key
+            global_counter += 1
+        
+        # Append the newly created dictionary to the list
+        new_list.append(new_dct)
+        
+    return {str(k): v for d in new_list for k, v in d.items()}
+estudios = {}
+for idx, tramo in enumerate(tramos):  # Assuming `tramos` is defined elsewhere
+    study_name = f"workforce_{idx}"
+    # Load the study
+    study = optuna.multi_objective.load_study(study_name=study_name, storage=storage)
+    # Extract all trials
+    all_trials = study.get_trials(deepcopy=False)    
+    estudios = estudios | {f"{study_name}":
+        { i: ( trial.values[0]/(trial.values[1]+trial.values[2]), trial.user_attrs.get('planificacion')) for i, trial in enumerate(all_trials) if trial.state == optuna.trial.TrialState.COMPLETE}
+        }
+mejores_configs = []
+
+for k,v in estudios.items():
+
+   tuple_list = [plan for trial, plan in v.items() ]
+   selected_tuple = max(tuple_list, key=lambda x: x[0])
+   
+   mejores_configs.append(selected_tuple[1])
+
+#%%
+
+dataset = DatasetTTP.desde_csv_atenciones("data/fonasa_monjitas.csv.gz")
+el_dia_real = dataset.un_dia("2023-05-15").sort_values(by='FH_Emi', inplace=False)
+
+
+planificacion = regenerate_global_keys(mejores_configs)#mejores_configs[0]
+prioridades   =  prioridad_x_serie(niveles_servicio_x_serie, 2, 1) 
+registros_SLA_real_con_worforce = simular(planificacion, niveles_servicio_x_serie, el_dia_real, prioridades)
+
+#%%
+
+
+
+registros_sla               = pd.DataFrame()
+tabla_atenciones         = el_dia_real[['FH_Emi', 'IdSerie', 'T_Esp']]
+tabla_atenciones.columns = ['FH_Emi', 'IdSerie', 'espera']
+SLA_index                = 0
+SLA_df                   = pd.DataFrame()
+for cliente_seleccionado in tabla_atenciones.iterrows():
+    
+
+    un_cliente   = pd.DataFrame(cliente_seleccionado[1][['FH_Emi', 'IdSerie', 'espera']]).T
+    registros_sla   =  pd.concat([registros_sla, un_cliente])#.reset_index(drop=True)
+    SLA_una_emision  =  pd.DataFrame(list(nivel_atencion_x_serie(registros_sla, niveles_servicio_x_serie).items()), columns=['keys', 'values'])
+
+    SLA_index+=1                        
+    SLA_una_emision['index']            = SLA_una_emision.shape[0]*[SLA_index]
+    SLA_una_emision['hora'] = un_cliente.FH_Emi[un_cliente.FH_Emi.index[0]].time().strftime('%H:%M:%S')#SLA_una_emision.time().strftime('%H:%M:%S')
+    SLA_df                              = pd.concat([SLA_df, SLA_una_emision], ignore_index=True)
+
+
+trajectoria_sla = SLA_df.pivot(index=['index', 'hora'], columns=['keys'], values='values').rename_axis(None, axis=1)
+
+trajectoria_sla.iloc[0:700]
+
+# # Instancia el generador
+# forecaster_ttp.generador = demandas_proyectadas_q.iterrows()
+
+# # COSA PARA GENERAR GRAFICOS A TIEMPOS ARBITRARIOS
+# for i, SERIE in enumerate(SERIES):
+
+#     pd.DataFrame({
+#         # Empaca series para lidear con el no tener el mismo largo
+#         f"{SERIE}_proyectada" : pd.Series(forecaster_ttp.demandas_proyectadas[f"{SERIE}_proyectada"]),
+#         f"{SERIE}_real"       : pd.Series(forecaster_ttp.demandas_proyectadas[f"{SERIE}_real"]),
+#     }).dropna().plot.line(title = f'Serie {SERIE}')
