03456259c78f934342f1cc48f978413029a9972c
Alejandro Acevedo <a.acevedo.aracena@gmail.com>
2023-10-26 20:13:44 -0300

    atributos_x_serie

--- dev/atributos_de_series.py
+++ dev/atributos_de_series.py
@@ -1,65 +1,6 @@
 #%%
 import numpy as np
-ids_series           = [6,  7,  11]
-sla_porcen_user      = [20, 60, 80]
-sla_corte_user       = [10*60, 15*60, 25*60]
-prioridades_user     = [1,2,3]
-pasos_user           = [2,4,5] 
-
-
-ids_series         = np.array(ids_series, dtype=int)
-sla_porcen_user    = np.array(sla_porcen_user, dtype=float)
-sla_corte_user     = np.array(sla_corte_user, dtype=int)
-prioridades_user   = np.array(prioridades_user, dtype=int)
-pasos_user         = np.array(pasos_user, dtype=int)
-atributos_x_series = [
-                     {'serie': s} for s in ids_series
-                     ]
-
 import itertools
-
-
-def update_atributos_x_series(atributos_x_series, sla_input_user, atributo):
-    
-
-    return  [atr_dict | 
-                        {atributo: np.random.randint(20,90) if sla_input_user is None else sla_p, 
-                        } for
-                        atr_dict, sla_p in 
-                        zip(atributos_x_series, itertools.repeat( None)  if sla_input_user is None else sla_input_user)]
-    
-    
-atributos_x_series = update_atributos_x_series(atributos_x_series, sla_porcen_user, 'sla_porcen')
-update_atributos_x_series(atributos_x_series, sla_corte_user, 'sla_corte')
-
-#%%
-if sla_porcen_user is None:
-    atributos_x_series = [atr_dict | 
-                            {'sla_porcen': np.random.randint(10,90) if sla_porcen_user is None else sla_p, 
-                            } for
-                            atr_dict, sla_p in zip(atributos_x_series, itertools.repeat( None)  if sla_porcen_user is None else sla_porcen_user)]
-else:
-    atributos_x_series = [{'serie': s, 
-                            'sla_porcen': p, 
-                            } for
-                            s,p in zip(ids_series, sla_porcen_user)]
-if sla_corte_user is None:
-    atributos_x_series = [{'serie': s, 
-                            'sla_corte':  np.random.randint(1,20*60), 
-                            } for
-                            s in ids_series]
-else:
-    atributos_x_series = [{'serie': s, 
-                            'sla_corte': p, 
-                            } for
-                            s,p in zip(ids_series, sla_corte_user)]
-
-
-atributos_x_series = [{'serie': s, 'sla_porcen': np.random.randint(10,90), 
-                        'sla_corte': np.random.randint(1,20*60),
-                        'prioridad':None, 
-                        'pasos':None } for
-                        s in ids_series]
 
 def rank_by_magnitude(arr):
     sorted_indices = sorted(enumerate(arr), key=lambda x: x[1], reverse=True)
@@ -74,22 +15,44 @@ def rank_by_magnitude(arr):
         rank_arr[index] = rank  # Store the rank at the original position
     return rank_arr
 
-def calcular_prioridad(porcentaje, espera, alpha:float=2, beta:float=1):
-    
+def calcular_prioridad(porcentaje, espera, alpha:float=2, beta:float=1):    
     return ((porcentaje**alpha)/(espera**beta))
 
+class atributos_de_series():
+    def __init__(self, ids_series:list):      
 
-prioridades_user     = [1,2,3]
+        self.ids_series         = np.array(ids_series, dtype=int)
+        self.atributos_x_series = [
+                                    {'serie': s} for s in ids_series
+                                    ]
+    def atributo(self, sla_input_user, atributo, low, high):
+        self.atributos_x_series = [atr_dict | 
+                {atributo: np.random.randint(low,high) if sla_input_user is None else sla_p, 
+                } for
+                atr_dict, sla_p in 
+                zip(self.atributos_x_series, itertools.repeat( None)  if sla_input_user is None else sla_input_user)]
+        
+        return  self
+    def prioridades(self, prioridad_input_user):
+        if prioridad_input_user is None:
+            prioridades_autom    = rank_by_magnitude([calcular_prioridad(s['sla_porcen'], s['sla_corte']) for s in self.atributos_x_series])
+            self.atributos_x_series = [s|{'prioridad':p} for s,p in zip(self.atributos_x_series, prioridades_autom)]
+            
+        else:
+            self.atributos_x_series = [s|{'prioridad':p} for s,p in zip(self.atributos_x_series, prioridad_input_user)]
+        return  self
+            
 
+def atributos_x_serie(ids_series, sla_porcen_user=None, sla_corte_user=None, pasos_user=None, prioridades_user=None):    
 
-prioridades_autom    = rank_by_magnitude([calcular_prioridad(s['sla_porcen'], s['sla_corte']) for s in atributos_x_series])
-#atributos_x_series  = [s|{'prioridad':p} for s,p in zip(atributos_x_series, prioridades_autom)]
+    att_x_s = atributos_de_series(ids_series = ids_series)
 
-pasos_user           = [2,4,5] 
-atributos_x_series   = [s|{'pasos':p} for s,p in zip(atributos_x_series, pasos_user)]
-
-
-
-
-
-atributos_x_series
+    return att_x_s.atributo(sla_porcen_user, 'sla_porcen', 50, 80).atributo(
+            sla_corte_user, 'sla_corte', 10*60, 30*60).atributo(
+                pasos_user, 'pasos',1,5).prioridades(prioridades_user).atributos_x_series
+            
+            
+#%% Ejemplo de uso
+if __name__ == "__main__":
+    atributos_x_serie(ids_series=[6,  7,  11], sla_porcen_user=[20, 60, 80], 
+                  sla_corte_user=[10*60, 15*60, 25*60], pasos_user=None, prioridades_user=[1,2,3])
